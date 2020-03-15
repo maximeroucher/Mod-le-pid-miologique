bdd296992252ce62b5ead3321a7a0124198951c1
Haven <34400034+maximeroucher@users.noreply.github.com>
2020-03-15 11:47:32 +0100

    commit

--- app/affichage.py
+++ app/affichage.py
@@ -1,38 +1,60 @@
-import matplotlib.pyplot as plt
-from matplotlib.patches import Polygon
-import shapefile
-import numpy as np
 import json
+import tkinter as tk
+import random
 
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import numpy as np
+import shapefile
 from dbfread import DBF, read
+from matplotlib.patches import Polygon
 
-""" table = read('110m_cultural/ne_110m_admin_0_countries.dbf', encoding='utf-8')
-tags = ["POP_EST", "GDP_MD_EST"]
 
-l = []
-for y in range(len(table)):
-    d = {table[y]["NAME_FR"]: {x : table[y][x] for x in tags}}
-    l.append(d)
-json.dump(l, open("Country_data.json", "w"))
-"""
-""" 
-sf = shapefile.Reader("110m_cultural/ne_110m_admin_0_scale_rank.shp")
+def format_data():
+    """ Ouvre le fichier des données mondiales et en récupère les données utiles à la simulation
+    ---
+    """
+    shapefile = './../res/110m_cultural/ne_110m_admin_0_countries.shp'
+    gdf = gpd.read_file(shapefile)
+    c_tag = gdf["ADM0_A3"]
+    c_geo = gdf["geometry"]
+    table = read('./../res/110m_cultural/ne_110m_admin_0_countries.dbf', encoding='utf-8')
+    tags = ["NAME_FR", "POP_EST", "GDP_MD_EST"]
+    l = []
+    for y in range(len(c_tag)):
+        for z in range(len(table)):
+            if table[z]["ADM0_A3"] == c_tag[y]:
+                d = {c_tag[y]: {x: table[z][x] for x in tags}}
+                if c_geo[z].__class__.__name__ is "MultiPolygon":
+                    shp = []
+                    for x in range(len(c_geo[z])):
+                        shp.append(list(c_geo[z][x].exterior.coords))
+                else:
+                    shp = list(c_geo[z].exterior.coords)
+                d[c_tag[y]]["geometry"] = shp
+                l.append(d)
+    json.dump(l, open("Country.json", "w"))
 
-pts = sf.shapes()[190].points
-pts_x, pts_y = [a for a, b in pts], [b for a, b in pts]
-plt.plot(pts_x, pts_y)
-plt.xlim(-185, 185)
-plt.ylim(-95, 95)
- """
-""" for x in range(len(sf)):
-    pts = sf.shapes()[x].points
-    pts_x, pts_y = [a for a, b in pts], [b for a, b in pts]
-    plt.plot(pts_x, pts_y)
-    plt.xlim(-185, 185)
-    plt.ylim(-95, 95) """
+
+
+def create_map():
+    """ Créer une carte du monde avec matplotlib
+    ---
+    """
+    sf = shapefile.Reader("./../res/110m_cultural/ne_110m_admin_0_scale_rank.shp")
+    for x in range(len(sf)):
+        pts = sf.shapes()[x].points
+        pts_x, pts_y = [a for a, b in pts], [b for a, b in pts]
+        b = random.randint(200, 245) / 255
+        g = random.randint(100, 170) / 255
+        r = random.randint(0, 70) / 255
+        plt.fill(pts_x, pts_y, facecolor=(r, g, b), edgecolor=(1, 1, 1), linewidth=1)
+    plt.show()
+
+
 
 def afficher_sim(data):
-    """ Afifiche le résultat de la simulation
+    """ Affiche le résultat de la simulation
     ---
     param : data (dict[label : list])
     """
--- app/sim.py
+++ app/sim.py
@@ -2,8 +2,7 @@ from affichage import afficher_sim
 
 class SIR:
 
-
-    def __init__(self, N, N0, t, tp, nb_iterations):
+    def __init__(self, N, N0, trans, tp, nb_iterations):
         """ Simulation selon le modèle S / I / R
         ---
         param :
@@ -13,15 +12,15 @@ class SIR:
             - t (float) la transmissibilité de la maladie
             - tp (int) le nombre de jour de maladie une fois infecté
             - nb_iterations (int) le nombre de jour de la simulation
-
         """
         self.N = N
         self.S = (N - N0) / N
         self.I = N0 / N
         self.R = 0
-        self.transmission = t
+        self.transmission = trans
         self.temps_maladie = tp
         self.nb_iterations = nb_iterations
+        self.pct_malade = 1 / self.temps_maladie
         self.i = []
         self.r = []
         self.s = []
@@ -32,8 +31,8 @@ class SIR:
         ---
         """
         dS = -self.transmission * self.I * self.S
-        dI = (self.transmission * self.S - 1 / self.temps_maladie) * self.I
-        dR = 1 / self.temps_maladie * self.I
+        dI = (self.transmission * self.S - self.pct_malade) * self.I
+        dR = self.pct_malade * self.I
         self.S += dS
         self.I += dI
         self.R += dR
@@ -42,6 +41,9 @@ class SIR:
     def simuler(self):
         """ Lance la simulation complète
         """
+        self.s.append(self.S)
+        self.i.append(self.I)
+        self.r.append(self.R)
         for x in range(self.nb_iterations):
             self.update()
             self.s.append(self.S)
@@ -52,5 +54,90 @@ class SIR:
     def format_sim(self):
         """ Formmatte les données de la simulation pour l'affichage
         """
-        return {'Sains': self.s, "Infectés": self.i, "Morts": self.r}
+        return {'Sains': self.s, "Infectés": self.i, "Morts / Guéris": self.r}
 
+
+    def __str__(self):
+        d = self.format_sim()
+        a = "\n\t- ".join([f"{x} : {d[x]}" for x in list(d.keys())])
+        return f"Population de {self.N} personnes.\nRésultat :\n\t- {a}"
+
+
+
+class SIRM:
+
+    def __init__(self, N, N0, transm, tp, l, nb_iterations):
+        """ Simulation selon le modèle S / I / R avec l'ajout de la léthalité de la maladie
+        ---
+        param :
+
+            - N (int) le nombre de personne dans la simulation
+            - N0 (int) le nombre de personne infectées au début de la simulation
+            - t (float, 0 <= t <= 1) la transmissibilité de la maladie
+            - tp (int) le nombre de jour de maladie une fois infecté
+            - l (float, 0 <= l <= 1) la léthalité de la maladie
+            - nb_iterations (int) le nombre de jour de la simulation
+        """
+        self.N = N
+        self.S = (N - N0) / N
+        self.I = N0 / N
+        self.R = 0
+        self.M = 0
+        self.transmission = transm
+        self.lethalite = l
+        self.temps_maladie = tp
+        self.nb_iterations = nb_iterations
+        self.pct_malade = 1 / self.temps_maladie
+        self.i = []
+        self.r = []
+        self.s = []
+        self.m = []
+
+
+    def update(self):
+        """ Avance d'un jour la simulation
+        ---
+        """
+        dS = -self.transmission * self.I * self.S
+        dI = (self.transmission * self.S - self.pct_malade) * self.I
+        dR = self.pct_malade * self.I * (1 - self.lethalite)
+        dM = self.pct_malade * self.I * self.lethalite
+        self.S += dS
+        self.I += dI
+        self.R += dR
+        self.M += dM
+
+
+    def simuler(self):
+        """ Lance la simulation complète
+        """
+        self.s.append(self.S)
+        self.i.append(self.I)
+        self.r.append(self.R)
+        self.m.append(self.M)
+        for x in range(self.nb_iterations):
+            self.update()
+            self.s.append(self.S)
+            self.i.append(self.I)
+            self.r.append(self.R)
+            self.m.append(self.M)
+
+
+    def format_sim(self):
+        """ Formmatte les données de la simulation pour l'affichage
+        """
+        return {'Sains': self.s, "Infectés": self.i, "Réscapés": self.r, "Morts": self.m}
+
+
+    def __str__(self):
+        d = self.format_sim()
+        a = "\n- " + "\n- ".join([f"{x} : {int(d[x][-1] * self.N)}" for x in list(d.keys())])
+        return f"Population de {self.N} personnes.\nRésultat :{a}"
+
+
+if __name__ == "__main__":
+    sim = SIRM(1000, 1, 0.5, 14, 0.02, 25)
+    sim.simuler()
+    print(sim)
+    out = sim.format_sim()
+    afficher_sim(out)
